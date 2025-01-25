e497bb61f646beade44714f4c2aa5b9180153b5c
Tarik elfahli <elfahli22@gmail.com>
2025-01-25 23:13:38 +0300

    name-change-app

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 
-export const APP_NAME = process.env.APP_NAME || 'NxtAmzn'
+export const APP_NAME = process.env.APP_NAME || 'TaRik_eLfahli'
 export const APP_SLOGAN = process.env.APP_SLOGAN || 'Spend less, enjoy more.'
 export const APP_DESCRIPTION = process.env.APP_DESCRIPTION || 'An Amazon clone built with Next.js and MongoDB.'
 
