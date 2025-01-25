f6533de5b74999344911005ad51793ae631e7e5c
Tarik elfahli <elfahli22@gmail.com>
2025-01-25 21:43:30 +0300

    create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+
+export const APP_NAME = process.env.APP_NAME || 'NxtAmzn'
+export const APP_SLOGAN = process.env.APP_SLOGAN || 'Spend less, enjoy more.'
+export const APP_DESCRIPTION = process.env.APP_DESCRIPTION || 'An Amazon clone built with Next.js and MongoDB.'
+
